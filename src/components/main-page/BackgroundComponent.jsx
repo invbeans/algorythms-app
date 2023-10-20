43fcb84346dcdb9efda4e2ceecf6c0beb45b0be4
invbeans <vlada.3234.vasileva@gmail.com>
2023-10-21 00:00:35 +0400

    main page background animation

--- src/components/main-page/BackgroundComponent.jsx
+++ src/components/main-page/BackgroundComponent.jsx
@@ -1,10 +1,37 @@
 import './BackgroundComponent.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import ElementComponent from '../../components/sort-page/ElementComponent'
+import { initArray, sortStep } from '../../slices/sortSlice';
 
 //i plan to do animation here so it is a seperated component
-export default function BackgroundComponent({children}) {
+export default function BackgroundComponent({ children }) {
+    let sortArray = useSelector((state) => state.sort.array);
+    let isDone = useSelector((state) => state.sort.doneSorting);
+    const dispatch = useDispatch();
+    let sortAlgorithm = 'BUBBLE_SORT';
+
+    useEffect(() => {
+        dispatch(initArray());
+    }, [sortAlgorithm, dispatch])
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            if (!isDone) {
+                dispatch(sortStep(sortAlgorithm));
+            }
+        }, 300)
+        return () => clearInterval(interval);
+    }, [sortAlgorithm, isDone, dispatch])
+
     return (
         <div className="background">
             {children}
+            <div className="sort-background">
+                {sortArray.map(num => {
+                    return <ElementComponent number={num} key={num}></ElementComponent>
+                })}
+            </div>
         </div>
     )
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,33 +1,8 @@
-//import { useDispatch, useSelector } from "react-redux"
 import BackgroundComponent from "../components/main-page/BackgroundComponent"
 import AlgorythmTypeButton from "../components/common/AlgorythmTypeButton"
-// import { EXAMPLE, changeValue, increment } from "../slices/example";
-// import { useEffect } from "react";
 import './MainPage.css';
 
 export default function MainPage() {
-    //REMOVE LATER (examples)
-
-    // const val = useSelector((state) => state.example.value);
-    // const ready = useSelector((state) => state.example.doneSorting);
-    // const dispatch = useDispatch();
-
-    // useEffect(() => {
-    //     let interval = setInterval(() => {
-    //         if(!ready) {
-    //             dispatch(increment(EXAMPLE))
-    //             console.log(ready)
-    //         }
-    //     }, 1000);
-    //     return () => clearInterval(interval);
-    // }, [ready, dispatch])
-
-    // function handleClick() {
-    //     dispatch(changeValue(5));
-    // }
-    /* <span>{val}</span>
-    <button onClick={handleClick}>Поменять валью в стейте</button> */
-
     return (
         <BackgroundComponent>
             <div className="grid-main">
